babcaff0550cf3da3dd1cf0d662001b6b08f6081
phtrebil <phptrebil@gmail.com>
2023-05-16 16:36:32 -0300

    começando a implementar databing no adapter parte 3

--- app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.kt
+++ app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.kt
@@ -8,6 +8,7 @@ import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.DiffUtil
@@ -15,6 +16,7 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.ceep.BR
 import br.com.alura.ceep.R
+import br.com.alura.ceep.databinding.ItemNotaBinding
 import br.com.alura.ceep.model.Nota
 import br.com.alura.ceep.ui.extensions.carregaImagem
 import kotlinx.android.synthetic.main.item_nota.view.*
@@ -28,7 +30,7 @@ class ListaNotasAdapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(context)
         val viewDataBinding =
-            DataBindingUtil.inflate<ViewDataBinding>(inflater, R.layout.item_nota, parent, false)
+           ItemNotaBinding.inflate(inflater, parent, false)
         return ViewHolder(viewDataBinding)
     }
 
@@ -38,7 +40,7 @@ class ListaNotasAdapter(
         }
     }
 
-    inner class ViewHolder(private val viewDataBinding: ViewDataBinding) :
+    inner class ViewHolder(private val viewDataBinding: ItemNotaBinding) :
         RecyclerView.ViewHolder(viewDataBinding.root) {
 
         private lateinit var nota: Nota
@@ -57,9 +59,7 @@ class ListaNotasAdapter(
 
         fun vincula(nota: Nota) {
             this.nota = nota
-            viewDataBinding.setVariable(BR.nota, nota)
-
-            campoImagem.carregaImagem(nota.imagemUrl)
+            viewDataBinding.nota = nota
 
 
         }
@@ -74,5 +74,9 @@ class ListaNotasAdapter(
 
         override fun areContentsTheSame(oldItem: Nota, newItem: Nota) = oldItem == newItem
 
+    }
+    @BindingAdapter("carregaImagem")
+    fun carregaImagemPorUrl(view: ImageView, url: String){
+        view.carregaImagem(url)
     }
 }
